1a2e489c5fa11aba252f6564ad02e2ad5d0927ca
kostas.kougios@gmail.com <kostas.kougios@googlemail.com>
2016-01-21 20:21:18 +0000

    fixing the index page & vehicle enquiry form

---
+++ app/controllers/IndexController.scala
@@ -0,0 +1,13 @@
+package controllers
+
+import play.api.mvc._
+
+class IndexController extends Controller
+{
+
+	def index = Action {
+		Ok(views.html.index())
+	}
+}
+
+
--- app/controllers/VehicleEnquiryController.scala
+++ app/controllers/VehicleEnquiryController.scala
@@ -1,13 +1,10 @@
 package controllers
 
-import javax.inject._
-
 import play.api.data.Form
 import play.api.data.Forms._
-import play.api.i18n._
 import play.api.mvc._
 
-class VehicleEnquiryController @Inject()(val messagesApi: MessagesApi) extends Controller with I18nSupport
+class VehicleEnquiryController extends Controller
 {
 
 	val vehicleEnquiryForm: Form[VehicleEnquiryForm] = Form {
@@ -17,22 +14,17 @@ class VehicleEnquiryController @Inject()(val messagesApi: MessagesApi) extends C
 		)(VehicleEnquiryForm.apply)(VehicleEnquiryForm.unapply)
 	}
 
-	def index = Action {
-		Ok(views.html.index(vehicleEnquiryForm))
+	def searchBegin = Action {
+		Ok(views.html.vehicleEnquiry(vehicleEnquiryForm))
 	}
 
 	def search() = Action { implicit request =>
-		// Bind the form first, then fold the result, passing a function to handle errors, and a function to handle succes.
 		vehicleEnquiryForm.bindFromRequest.fold(
-			// The error function. We return the index page with the error form, which will render the errors.
-			// We also wrap the result in a successful future, since this action is synchronous, but we're required to return
-			// a future because the person creation function returns a future.
 			errorForm => {
-				Ok(views.html.index(errorForm))
+				Ok(views.html.vehicleEnquiry(errorForm))
 			},
-			// There were no errors in the from, so create the person.
 			person => {
-				Redirect(routes.VehicleEnquiryController.index)
+				Redirect(routes.VehicleEnquiryController.searchBegin)
 			}
 		)
 	}
