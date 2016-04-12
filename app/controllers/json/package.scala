654aabdf1d3d20861b891b12ffc97331c0471567
kostas.kougios@gmail.com <kostas.kougios@googlemail.com>
2016-04-12 11:30:07 +0100

    implementing a rest api

--- app/controllers/json/package.scala
+++ app/controllers/json/package.scala
@@ -12,6 +12,10 @@ package object json
 {
 	implicit val FormReads = Json.reads[VehicleEnquiry]
 	implicit val FormWrites = Json.writes[VehicleEnquiry]
+	// Note: json schema should not be the domain model, but here this are so
+	// simple and we'll use the domain model class Vehicle. If the json api
+	// and domain class start to diverse, a new json schema class should replace
+	// Vehicle in the declarations below:
 	implicit val VehicleReads = Json.reads[Vehicle]
 	implicit val VehicleWrites = Json.writes[Vehicle]
 
